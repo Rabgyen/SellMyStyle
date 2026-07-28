df554cb6fb9f04aa71c1e01e297e4451e76b45c0
Rabgyen <rabmoktan10@gmail.com>
2026-07-28 16:01:04 +0545

    Fetched data from the database

--- src/context/CategoryContext.jsx
+++ src/context/CategoryContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const CategoryContext = createContext();
 
@@ -7,9 +8,26 @@ export const useCategoryContext = () => useContext(CategoryContext);
 export const CategoryProvider = ({ children }) => {
   const [category, setCategory] = useState("all");
   const [searchTerm, setSearchTerm] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const categoryResponse = await axios.get(
+          "http://localhost:5000/product/product_categories",
+        );
+        setCategories(categoryResponse.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCategory();
+  }, []);
 
   return (
-    <CategoryContext.Provider value={{ category, setCategory, searchTerm, setSearchTerm }}>
+    <CategoryContext.Provider
+      value={{ category, setCategory, searchTerm, setSearchTerm, categories, setCategories }}
+    >
       {children}
     </CategoryContext.Provider>
   );
